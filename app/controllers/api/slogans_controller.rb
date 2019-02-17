a5bd53e9e8379e0c94d030aefc9cf13716b14d12
swedkg <dimitrios.giannopoulos@gmail.com>
2019-02-17 22:36:16 +0100

    slogan api woorking

--- app/controllers/api/slogans_controller.rb
+++ app/controllers/api/slogans_controller.rb
@@ -1,7 +1,27 @@
 class Api::SlogansController < ApplicationController
-  def index
-    @slogans = Slogan.all
+  skip_before_action :verify_authenticity_token
 
+  def index
+    @slogans = Api::Slogan.all
     render json: @slogans, status: :ok
+  end
+
+  def create
+    @trace = Api::Slogan.find_by(email: slogan_params[:email])
+    puts "trace: ", @trace
+    if @trace.nil?
+      puts "not found, write to db"
+      @new_slogan = Api::Slogan.new(slogan_params)
+      if @new_slogan.save
+        head (:created)
+      end
+    else
+      puts "already there, send error message"
+      head(:ok)
+    end
+  end
+
+  def slogan_params
+    params.permit(:first_name, :last_name, :email, :slogan)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :slogans
   end
 
-  resources :riders
+  # resources :riders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
